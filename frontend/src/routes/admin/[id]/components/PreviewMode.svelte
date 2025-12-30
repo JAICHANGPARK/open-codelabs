beefcb0a5b0267bf8deda68091c63afaea93bd2f
Dreamwalker <aristojeff@gmail.com>
2025-12-30 11:24:29 +0900

    Create PreviewMode.svelte

---
+++ frontend/src/routes/admin/[id]/components/PreviewMode.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let { renderedContent } = $props<{ renderedContent: string }>();
+</script>
+
+<div class="prose dark:prose-invert prose-blue max-w-none markdown-body">
+    {@html renderedContent}
+</div>
